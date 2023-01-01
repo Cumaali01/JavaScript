a488d55e89222716f30a8d9def2b8e97e901c7ca
Cumaali01 <cumaalicelik38@gmail.com>
2023-01-01 16:43:09 +0300

    operators

---
+++ JS-03-operators/operators.js
@@ -0,0 +1,17 @@
+//Calculation
+
+let num1= +prompt('Enter first number');
+let num2= +prompt('Enter Number Two');
+
+console.log(`${num1} x ${ num2} is ${num1*num2},\n${num1}/${num2} is ${num1/num2}`);
+
+//Area of Rectange
+
+let a = +prompt('Enter a side');
+let b = +prompt('Enter b side');
+console.log(`Area of the rectangular is ${a*b} sqm`);
+
+// Area of circle
+
+let radius = +prompt('Enter the radius');
+console.log(`Area of the circle is ${Math.PI*radius*radius} sqm`);
