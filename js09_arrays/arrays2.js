c9c86ba153788386a03bc50e2dd3f93d11628a4b
Cumaali01 <cumaalicelik38@gmail.com>
2023-01-18 20:41:20 +0300

    arrays

---
+++ js09_arrays/arrays2.js
@@ -0,0 +1,16 @@
+let selectedColors = ["red", "blue"];
+selectedColors[2] = "purple";
+selectedColors[3] = 25;
+console.log(selectedColors, typeof selectedColors);
+
+console.log(selectedColors.length);
+
+let user = +prompt("Enter numbers");
+
+const numbers = [1, 3, 5, 7, 9];
+
+if ((user = numbers[x])) {
+  console.log("Great, you found the number: " + user);
+} else {
+  console.log("The number is wrong");
+}
